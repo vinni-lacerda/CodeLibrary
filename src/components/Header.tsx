2d5cb034e42804516a359e0b1981d14c92b726c4
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-21 14:15:09 -0300

    feat: Add Header component with CSS styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+const Header = () => {
+    return <div className="header-container">
+        <img src="./Logo.png" alt="" className="logo-img"/>
+        <p className="btn-premium">Premium</p>
+    </div>
+}
+
+export default Header;
