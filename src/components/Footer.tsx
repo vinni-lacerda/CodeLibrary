0ef5fe7781ca9193cd163eca8ab4c3356b2e1527
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-22 00:15:45 -0300

    feat: Add Footer component with copyright message.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () =>{
+    return <div>
+        <p className="text-center font-bold text-[#00A2F7]">Todos os direitos reservados © CodeLibrary</p>
+    </div>
+}
+
+export default Footer;
