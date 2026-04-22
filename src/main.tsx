ec53fc8f477742e4d5512f4741932c4a4cd6f872
vinni-lacerda <vini.lacerda@hotmail.com>
2026-04-22 00:19:56 -0300

    refactor: main component structure, replacing App with Header, Filter, and Footer.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Footer from './components/Footer.tsx'
+import Header from './components/Header.tsx'
+import Filter from './components/Filter.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='flex flex-col h-screen bg-[#F4FBFF]'>
+      <Header />
+      <div className='flex-1 max-w[1200px] m-auto w-full'>
+        <Filter 
+        title ='Busque o conteúdo que deseja assitir hoje'
+        placeholder='Curso de react'
+        />
+      </div>
+      <Footer />
+    </div>
   </StrictMode>,
 )
